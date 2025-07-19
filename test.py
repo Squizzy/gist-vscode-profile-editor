f0b0c33a2f7abc32579a59818d9eb8d7c19bbf5a
Squizzy <crazy_squirrel@yahoo.com>
2025-07-19 23:10:26 +0100

    test file

---
+++ test.py
@@ -0,0 +1,15 @@
+from modules.FileIO import FileIO
+
+def main():
+    file_io = FileIO()
+    # file_io.load_vscode_profile_to_modify()
+    # file_io._get_vscode_registered_vscode_profiles()
+    file_io._get_local_extensions_list_from_folders()
+    # file_io._get_local_extensions_list_from_json_file()
+    file_io._get_extensions_settings_keys()
+    # print(file_io.profile_data)
+    # file_io.load_vscode_profile_to_modify()
+    # print(file_io.profile_data)
+
+if __name__ == "__main__":
+    main()
