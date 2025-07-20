140eb82b42ce6e975d60c29f3abf24e116cfda7d
Squizzy <crazy_squirrel@yahoo.com>
2025-07-20 21:15:41 +0100

    small tool used at testing time, should really have been test...

--- test.py
+++ test.py
@@ -4,12 +4,14 @@ def main():
     file_io = FileIO()
     # file_io.load_vscode_profile_to_modify()
     # file_io._get_vscode_registered_vscode_profiles()
-    file_io._get_local_extensions_list_from_folders()
+    # file_io._get_local_extensions_list_from_folders()
     # file_io._get_local_extensions_list_from_json_file()
-    file_io._get_extensions_settings_keys()
+    # file_io._get_extensions_settings_keys()
     # print(file_io.profile_data)
     # file_io.load_vscode_profile_to_modify()
     # print(file_io.profile_data)
+    # file_io._list_gist_profiles()
+    file_io.load_gist_profile_data("<some existing GITHUB gist key>")
 
 if __name__ == "__main__":
     main()
